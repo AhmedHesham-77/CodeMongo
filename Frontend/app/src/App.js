a5d3b12f317bb7883143de077b7df62c959f87d9
Ahmed Hesham <ahmed.hesham0707@outlook.com>
2023-12-07 02:50:44 +0200

    Update

--- Frontend/app/src/App.js
+++ Frontend/app/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+// import SignIn from "./components/auth/SignIn";
+// import SignUp from "./components/auth/SignUp";
+import Home from "./components/Home";
+import * as ROUTES from "./constants/routes";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Routes>
+          <Route path={ROUTES.HOME} element={<Home />} >
+          </Route>
+          {/* <Route path={ROUTES.SIGN_IN} element={<SignIn />} />
+          <Route path={ROUTES.SIGN_UP} element={<SignUp />} /> */}
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
