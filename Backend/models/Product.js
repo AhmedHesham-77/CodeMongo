0ec6f0b6aaf92d9fbe8f28b6e3e09cd35d26f515
Ahmed Hesham Abd Elmohiy <96230470+ah07072003@users.noreply.github.com>
2023-12-08 01:41:53 +0200

    upload cell code

--- Backend/models/Product.js
+++ Backend/models/Product.js
@@ -13,6 +13,10 @@ const productSchema = new mongoose({
         type: String,
         required: true,
     },
+    price: {
+        type: Number,
+        required: true,
+    },
     user:{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
